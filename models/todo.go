c6699298be0b31e9489a8f758431c3b00349046a
alip <alifrizkipambudi@gmail.com>
2019-07-23 21:08:13 +0700

    remove old project structure

--- models/todo.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// Todo model
-type Todo struct {
-	TodoID string `json:"todo_id"`
-	Title  string `json:"title"`
-	Detail string `json:"detail"`
-	IsDone bool   `json:"is_done"`
-}
